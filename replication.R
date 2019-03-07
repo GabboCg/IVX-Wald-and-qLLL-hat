e0edcb7bf2aa5ccfde7a26aa3d92bd71548e680f
Gabriel Cabrera <gcabrerag@fen.uchile.cl>
2019-03-06 22:29:08 -0300

    compute out-of-sample forecasts

--- replication.R
+++ replication.R
@@ -93,12 +93,13 @@ for(i in seq_along(h)){
       
       X_i_j <- as.matrix(GW_predictor_z[1:(NROW(GW_predictor_z)-h[i]), j])
       Y_i_j <- 100*r_h[2:(NROW(r_h)-(h[i]-1)), i]
-      results_i_j <- newey_west(Y_i_j , X_i_j, h[i])
+      # results_i_j <- newey_west(Y_i_j , X_i_j, h[i])
       
       results_i_j_lm <- lm(100*r_h[2:(NROW(r_h)-(h[i]-1)), i] ~ X_i_j)
+      results_i_j_lm_coef <- coeftest(results_i_j_lm, vcov.=NeweyWest(results_i_j_lm, lag=h[i], adjust=FALSE, verbose = TRUE, prewhite = FALSE))
       
-      beta_hat[j,,i] <- cbind(results_i_j[[1]][2],
-                              results_i_j[[1]][2]/(sqrt(results_i_j[[2]][2,2])), # coef(summary(results_i_j))[, "t value"][2]
+      beta_hat[j,,i] <- cbind(results_i_j_lm_coef[2,1],
+                              results_i_j_lm_coef[2,3], # coef(summary(results_i_j))[, "t value"][2]
                               NaN, 
                               summary(results_i_j_lm)$r.squared*100)
       
@@ -107,7 +108,7 @@ for(i in seq_along(h)){
   }
 }
 
-round(beta_hat, 4)
+round(beta_hat, 2)
 
 # Compute fixed-regressor wild bootstrap p-values ------------------------------
 X_sink <- data.frame(cbind(GW_predictor_z, -SII)) %>% 
@@ -118,14 +119,14 @@ epsilon_hat <- as.matrix(results_sink$residuals)
 B <- 1000
 beta_hat_tstat_star <- array(NaN, c(B, NCOL(GW_predictor) + 1, length(h)))  
 
-set.seed(1234)
-lagWild <- matrix(NaN, nrow = 10000, ncol = 12)
-runif(length(lagWild), 0,1)
+saved.seed <- sample(1e9,1)
+set.seed(saved.seed)
 
 for(b in 1:B){
   
   print(b)
-  u_star_b <- as.numeric(runif(NROW(r_h)-1, 0,1))
+  
+  u_star_b <- as.numeric(rnorm(NROW(r_h)-1, 0,1))
   r_star_b <- rbind(equity_risk$r[1], mean(equity_risk$r) + epsilon_hat*u_star_b)
   r_h_star_b <- matrix(NaN, NROW(equity_risk$r), length(h)) 
   
@@ -145,14 +146,15 @@ for(b in 1:B){
        
         X_i_j <- as.matrix(GW_predictor_z[1:(NROW(GW_predictor_z)-1-(h[j]-1)), i])
         Y_i_j <- as.vector(100*r_h_star_b[2:(NROW(r_h_star_b)-(h[j]-1)), j])
-        results_i_j_star_b <- newey_west(Y_i_j , X_i_j, h[j])
         
-        beta_hat_tstat_star[b,i,j] <- results_i_j_star_b[[1]][2]/(sqrt(results_i_j_star_b[[2]][2,2])) # coef(summary(results_i_j))[, "t value"][2]
+        results_i_j_star_b_lm <- lm(Y_i_j ~ X_i_j)
+        results_i_j_star_b <- coeftest(results_i_j_star_b_lm, vcov.=NeweyWest(results_i_j_star_b_lm, lag = h[j], adjust=FALSE, verbose = TRUE, prewhite = FALSE))
+    
+        beta_hat_tstat_star[b,i,j] <- results_i_j_star_b[2,3] # results_i_j_star_b[[1]][2]/(sqrt(results_i_j_star_b[[2]][2,2])) or coef(summary(results_i_j))[, "t value"][2]
         
       }
     }
   }
-  
 }
 
 for(j in 1:length(h)){
@@ -167,4 +169,60 @@ colnames(beta_hat) <- c("Beta", "t_stat", "p_value", "R2")
 rownames(beta_hat) <- c("DP","DY","EP","DE","RVOL","BM","NTIS",
                         "TBL","LTY","LTR","TMS","DFY","DFR","INFL")
 
-round(beta_hat, 4)
+round(beta_hat, 2)
+
+# Take care of out-of-sample preliminaries -------------------------------------
+
+T <- length(equity_risk$r)
+in_sample_end <- 1989
+R <- (in_sample_end - 1972)*12 # in-sample period
+P <- T - R
+
+FC_PM <- matrix(NaN, nrow = P, ncol = 1)
+FC_PR <- array(NaN, c(P, NCOL(GW_predictor), NROW(h)))
+
+# Compute out-of-sample forecasts ----------------------------------------------
+
+for(p in 1:P){
+  
+  print(p)
+  
+  FC_PM[p] <- mean(equity_risk$r[1:(R+(p-1))])
+  
+  for(j in 1:NROW(h)){
+    for(i in 1:NCOL(GW_predictor)){
+      
+      X_i_j_p <- GW_predictor[1:(R+(p-1)-h[j]),i]
+      results_i_j_p <- lm(r_h[2:(R+p-h[j]),j] ~ X_i_j_p)
+      FC_PR[p,i,j] <- cbind(1, GW_predictor[R+(p-1),i])%*%results_i_j_p$coeff
+      
+    }
+  }
+}
+
+# evaluate forecasts -----------------------------------------------------------
+
+R2OS_PR <- array(NaN, c(NCOL(GW_predictor),2,NROW(h)))
+
+for(j in 1:length(h)){
+  
+  actual_j <- r_h[(R+1):(NROW(r_h)-(h[j]-1)),j]
+  u_PM_j <- actual_j - FC_PM[1:(NROW(FC_PM)-(h[j]-1))] 
+  u_PR_j <- kronecker(matrix(1, nrow = 1, ncol = NCOL(FC_PR)), actual_j) - FC_PR[1:(NROW(FC_PR)-(h[j]-1)),,j] 
+  
+  MSFE_PM_j <- mean(u_PM_j^2)
+  MSFE_PR_j <- apply(u_PR_j^2, 2, mean)
+  
+  R2OS_PR_j <- 100*(1-MSFE_PR_j/MSFE_PM_j)
+  R2OS_PR[,1,j] = t(R2OS_PR_j)
+  
+  for(i in 1:NCOL(GW_predictor)){
+    
+   f_CW_i_j <- u_PM_j^2 - u_PR_j[,i]^2 + (FC_PM[1:(NROW(FC_PM)-(h[j]-1))] - FC_PR[1:(NROW(FC_PR)-(h[j]-1)),i,j])^2
+   result_CW_i_j <- lm(f_CW_i_j ~ matrix(1, nrow = NROW(f_CW_i_j)))
+   R2OS_PR[i,2,j] <- coeftest(result_CW_i_j, vcov.=NeweyWest(result_CW_i_j, lag=h[j], adjust=FALSE, verbose = TRUE, prewhite = FALSE))[3]
+   
+  }
+}
+
+round(R2OS_PR, 2)
